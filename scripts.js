8670be5c349290612ab370fa9f5252894efc790d
Dmitry Bondarchuk <dimitrywebdeveloper@gmail.com>
2022-04-25 20:46:40 +0300

    set the movement of figures

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const board = document.getElementById("board");
 const start_button = document.getElementById("start_btn");
 
-start_button.addEventListener("click", start);
+//start_button.addEventListener("click", start);
 
 const count_cells = 64;
 
@@ -13,18 +13,17 @@ const queen = "♛";
 const king = "♚";
 
 let figureschess_pieces = [
-  rook,horse,bishop,queen,king,bishop,horse,rook,
-  pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
-  "", "", "", "", "", "", "", "",
-  "", "", "", "", "", "", "", "",
-  "", "", "", "", "", "", "", "",
-  "", "", "", "", "", "", "", "",
-  pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
-  rook,horse,bishop,queen,king,bishop,horse,rook,
+  [rook,horse,bishop,queen,king,bishop,horse,rook],
+  [pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", king, "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  [pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn],
+  [rook,horse,bishop,queen,king,bishop,horse,rook],
 ];
 
 let dragged;
-let target_piece;
 
 /* events fired on the draggable target */
 board.addEventListener("drag", function (event) {
@@ -56,12 +55,6 @@ board.addEventListener(
   (event) => {
     // prevent default to allow drop
     event.preventDefault();
-
-    if(event.target.id === "white_♟" && dragged.id === "black_♟" ||
-      event.target.id === "black_♟" && dragged.id === "white_♟"){
-        console.log("target")
-        target_piece = event.target;
-    }
   },
   false
 );
@@ -70,9 +63,9 @@ board.addEventListener(
   "dragenter",
   (event) => {
     // highlight potential drop target when the draggable element enters it
-    if (event.target.id == "cell") {
+    /*if (event.target.id == "cell") {
       event.target.style.background = "rgb(109, 64, 155)";
-    }
+    }*/
   },
   false
 );
@@ -81,9 +74,9 @@ board.addEventListener(
   "dragleave",
   (event) => {
     // reset background of potential drop target when the draggable element leaves it
-    if (event.target.id == "cell") {
+    /*if (event.target.id == "cell") {
       event.target.style.background = "";
-    }
+    }*/
   },
   false
 );
@@ -97,16 +90,87 @@ board.addEventListener(
 
     const dragged_target = () => {
       event.target.style.background = "";
-      /*if(target_piece){
-        target_piece.parentNode.removeChild(target_piece);
-      }*/
       dragged.parentNode.removeChild(dragged);
       event.target.appendChild(dragged);
     }
 
     const is_right_step = () => {
+      console.log(dragged.id.slice(0,5), event.target.id.slice(0,5))
+      if(dragged.id.slice(0,5) === "white" && event.target.id.slice(0,5) === "white"
+      || dragged.id.slice(0,5) === "black" && event.target.id.slice(0,5) === "black") {
+        return false;
+      } // проверяю не является ли ход попыткой побить свою фигуру.
+
       if(dragged.id === `white_${pawn}` || dragged.id === `black_${pawn}`){
-        return true;
+        if(dragged.id === `black_${pawn}` &&
+          dragged.parentNode.id.slice(7, 8) === event.target.id.slice(7, 8) &&
+          (event.target.id.indexOf('y=6') !== -1 ||
+          event.target.id.indexOf('y=5') !== -1) &&
+          dragged.parentNode.id.slice(11) > event.target.id.slice(11)) {
+          return true;
+        } else if(dragged.id === `white_${pawn}` &&
+          dragged.parentNode.id.slice(7, 8) === event.target.id.slice(7, 8) &&
+          (event.target.id.indexOf('y=3') !== -1 ||
+          event.target.id.indexOf('y=4') !== -1) &&
+          dragged.parentNode.id.slice(11) > event.target.id.slice(11)) {
+          return true;
+        } else if(dragged.id === `black_${pawn}` &&
+        dragged.parentNode.id.slice(7, 8) === event.target.id.slice(7, 8) &&
+        +dragged.parentNode.id.slice(11) - 1 == event.target.id.slice(11)) {
+          return true;
+        } else if(dragged.id === `white_${pawn}` &&
+        dragged.parentNode.id.slice(7, 8) === event.target.id.slice(7, 8) &&
+        +dragged.parentNode.id.slice(11) + 1 == event.target.id.slice(11)){
+          return true;
+        }
+      } else if(dragged.id === `white_${bishop}` || dragged.id === `black_${bishop}`) {
+        if(Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11))) {
+          return true;
+        }
+      } else if(dragged.id === `white_${horse}` || dragged.id === `black_${horse}`) {
+        if(
+          (Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 1 &&
+          Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 2) ||
+          (Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 2 &&
+          Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 1)
+        ) {
+          return true;
+        }
+      } else if(dragged.id === `white_${rook}` || dragged.id === `black_${rook}`) {
+        if(Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 0 && Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) !== 0 ||
+        Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) !== 0 && Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 0){
+          return true;
+        }
+      } else if(dragged.id === `white_${queen}` || dragged.id === `black_${queen}`) {
+        if(Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) ||
+           Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 0 && Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) !== 0 ||
+           Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) !== 0 && Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 0
+        ) {
+          return true;
+        }
+      } else if(dragged.id === `white_${king}` || dragged.id === `black_${king}`) {
+        if(
+          (Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 1 &&
+          Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 1) ||
+          (Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 1 &&
+          Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 0) ||
+          (Math.abs(+event.target.id.slice(7, 8) - +dragged.parentNode.id.slice(7, 8)) === 0 &&
+          Math.abs(+event.target.id.slice(11) - +dragged.parentNode.id.slice(11)) === 1)
+        ) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    const is_right_attack = () => {
+      if(dragged.id === `white_${pawn}` || dragged.id === `black_${pawn}`){
+        if(dragged.id === `black_${pawn}` && dragged.parentNode.id.slice(7, 8) && event.target.id.slice(11)) {
+          return true;
+        } else if(dragged.id === `white_${pawn}` && dragged.parentNode.id.slice(7, 8) && event.target.id.slice(11)) {
+          return true;
+        }
       } else if(dragged.id === `white_${bishop}` || dragged.id === `black_${bishop}`) {
         return true;
       } else if(dragged.id === `white_${horse}` || dragged.id === `black_${horse}`) {
@@ -118,16 +182,13 @@ board.addEventListener(
       } else if(dragged.id === `white_${king}` || dragged.id === `black_${king}`) {
         return true;
       }
-
-      return false;
     }
 
     if(is_right_step()) {
-      if(dragged && target_piece){
-        console.log("target", target_piece)
-        dragged_target();
-
-        target_piece = null;
+      if(dragged){
+        if(is_right_attack()){
+          dragged_target();
+        }
       } else {
         dragged_target();
       }
@@ -141,7 +202,7 @@ const create_board = () => {
   let switch_parity_y = true;
 
   let x = 0;
-  let y = 0;
+  let y = 1;
 
   for (let i = 0; i < count_cells; i++) {
     let div = document.createElement("div");
@@ -171,17 +232,17 @@ const create_board = () => {
       }
     }
 
-    div.id = "cell";
+    div.id = `cell_x=${x}_y=${y}`;
 
     if (y < 4) {
       piece.classList.add("white_piece");
-      piece.id = `white_${figureschess_pieces[i] || "piece"}`;
+      piece.id = `white_${figureschess_pieces[y - 1][x - 1] || "piece"}`;
     } else {
       piece.classList.add("black_piece");
-      piece.id = `black_${figureschess_pieces[i] || "piece"}`;
+      piece.id = `black_${figureschess_pieces[y - 1][x - 1] || "piece"}`;
     }
 
-    piece.innerText = figureschess_pieces[i];
+    piece.innerText = figureschess_pieces[y - 1][x - 1];
     piece.setAttribute("draggable", "true");
 
     let wrap_div = board.appendChild(div);
